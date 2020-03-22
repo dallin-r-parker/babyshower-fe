4419eae528db0b5b7b5aa47a10e81565dacad02f
dallin parker <13424017+dallin-r-parker@users.noreply.github.com>
2020-03-22 12:43:25 -0400

    globals are working

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,18 +1,17 @@
+type FlexAlignmentValues =
+  | 'center'
+  | 'start'
+  | 'end'
+  | 'flex-start'
+  | 'flex-end'
+  | 'left'
+  | 'right';
+
+type FlexDistributedValues =
+  | 'space-between'
+  | 'space-around'
+  | 'space-evenly'
+  | 'stretch';
 declare namespace CSS {
-  type FlexAlignmentValues =
-    | "center"
-    | "start"
-    | "end"
-    | "flex-start"
-    | "flex-end"
-    | "left"
-    | "right";
-
-  type FlexDistributedValues =
-    | "space-between"
-    | "space-around"
-    | "space-evenly"
-    | "stretch";
-
   type JustifyConent = FlexAlignmentValues | FlexDistributedValues;
 }
