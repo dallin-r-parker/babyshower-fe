12ac5ee3decbcf3d9e1ece647366a5c503a197b0
dallin parker <13424017+dallin-r-parker@users.noreply.github.com>
2020-03-22 00:26:07 -0400

    WIP: making a cloud component have playground working

--- src/components/Star/index.tsx
+++ src/components/Star/index.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 //NOTE: changing the NUMBER in the "tranform" will make the star "skewed"
 
-export const Star = styled.i`
+export const Star = styled.div`
   position: relative;
 
   display: inline-block;
